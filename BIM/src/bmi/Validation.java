2c3146f623377bd4e6a07f42f1c737fce0d300da
doanhang1508 <doanthuhanggt@gmail.com>
2025-10-11 09:37:38 +0700

    Update Validation.java

--- BIM/src/bmi/Validation.java
+++ BIM/src/bmi/Validation.java
@@ -11,23 +11,26 @@ import java.util.Scanner;
  * @author Thanh Hang
  */
 public class Validation {
+
     private static final Scanner sc = new Scanner(System.in);
-     public static int inputInteger(String msg, int min, int max){
-        while(true){
-            try{
+
+    public static int inputInteger(String msg, int min, int max) {
+        while (true) {
+            try {
                 System.out.println(msg);
-            int value = Integer.parseInt(sc.nextLine().trim());
-            if(value<min||value>max){
-                System.out.println("Value must be between " + min + " and " + max);
-            }else{
-                return value;
-            }
-            }catch(NumberFormatException e){
+                int value = Integer.parseInt(sc.nextLine().trim());
+                if (value < min || value > max) {
+                    System.out.println("Value must be between " + min + " and " + max);
+                } else {
+                    return value;
+                }
+            } catch (NumberFormatException e) {
                 System.err.println("Invalid input! Please enter a number");
             }
         }
     }
-      public static double inputDouble(String msg) {
+
+    public static double inputDouble(String msg) {
         while (true) {
             System.out.print(msg);
             String s = sc.nextLine().trim();
@@ -38,26 +41,29 @@ public class Validation {
             }
         }
     }
-      public static double inputPositiveDouble(String prompt) {
+
+    public static double inputDoubleInRange(String prompt, double min, double max) {
         while (true) {
             double v = inputDouble(prompt);
-            if (v > 0) return v;
-            System.err.println("Value must be > 0. Try again.");
+            if (v >= min && v <= max) {
+                return v;
+            }
+            System.err.printf("Value must be meters", min, max);
         }
     }
 
     // Chấp nhận: + - * / ^ =  ( '=' để kết thúc tính )
-    public static char inputOperator(String msg, boolean allowEqual) {
+    public static char inputOperator(String msg) {
         while (true) {
             System.out.print(msg);
             String s = sc.nextLine().trim();
             if (s.length() == 1) {
                 char c = s.charAt(0);
-                if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || (allowEqual && c == '=')) {
+                if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' ||  c == '=') {
                     return c;
                 }
             }
-            System.err.println("Operator must be one of: + - * / ^" + (allowEqual ? " =" : ""));
+            System.err.println("Operator must be one of: + - * / ^ =" );
         }
     }
 }
