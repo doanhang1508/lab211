e83f6aee5499fb94f501b81f98ee81af3df7d59f
doanhang1508 <doanthuhanggt@gmail.com>
2025-10-11 16:57:19 +0700

    Update Validation.java

--- CheckForm/src/checkform/Validation.java
+++ CheckForm/src/checkform/Validation.java
@@ -14,84 +14,92 @@ import java.util.regex.Pattern;
  * @author Thanh Hang
  */
 public class Validation {
+
     private static Scanner sc = new Scanner(System.in);
-    static String DATE_REGEX="\\d{2}/\\d{2}/\\d{4}";
-    static String EMAIL_REGEX="^[A-Za-z0-9]+@[A-Za-z]+\\.[A-Za-z]+$";
-    static String PHONE_REGEX="\\d+";
-    public static String checkPhone(){
-        while(true){
+    static String DATE_REGEX = "\\d{2}/\\d{2}/\\d{4}";
+    static String EMAIL_REGEX = "^[A-Za-z0-9]+@[A-Za-z]+\\.[A-Za-z]+$";
+    static String PHONE_REGEX = "\\d+";
+
+    public static String checkPhone() {
+        while (true) {
             System.out.print("Phone: ");
             String phone = sc.nextLine().trim();
             if (!phone.matches("\\d+")) {
                 System.out.println("Phone number must be number.");
-            }
-            else if (phone.length() != 10) {
+            } else if (phone.length() != 10) {
                 System.out.println("Phone number have 10 digit.");
+            } else {
+                return phone;
             }
-            else return phone;
         }
     }
-    public static String checkEmail(){
-        while(true){
+
+    public static String checkEmail() {
+        while (true) {
             System.out.print("Email: ");
             String email = sc.nextLine().trim();
-            if(!email.matches(EMAIL_REGEX)){
+            if (!email.matches(EMAIL_REGEX)) {
                 System.out.println("Invalid email");
+            } else {
+                return email;
             }
-            else return email;
         }
     }
-    public static String checkDate(){
-      while(true){
-          System.out.print("Date: ");
-         String date = sc.nextLine().trim();
-         if(!date.matches(DATE_REGEX)){
-             System.out.println("Date must be format (dd/MM/yyyy)");
-             continue;
-        }
-             
-        String[] part = date.split("/");
-        int day = Integer.parseInt(part[0]);
-        int month = Integer.parseInt(part[1]);
-        int year = Integer.parseInt(part[2]);
-        boolean isValid = true;
-        if(month<1 || month >12){
-            System.out.println("Month must be from 1 to 12");
-            isValid = false;
-            
-        }
-        if (month >= 1 && month <= 12) {
-        int[] dayOfMonth={31,28,31,30,31,30,31,31,30,31,30,31};
-        if(year % 400 ==0 ||(year%4==0 && year%100!=0)){
-            dayOfMonth[1]=29;
-        }
-        
-        if(day<1||day>dayOfMonth[month-1]){
-            System.out.println("Invalid day!");
-              isValid = false;
-        }
-        }
-        if(isValid) return date;
-         
-      }
-    }
-     public static String inputWithValidation(String type) {
-    while (true) {
-        String msg = "";
 
-        switch (type.toLowerCase()) {
-            case "phone":
-                msg = checkPhone();
-                break;
-            case "email":
-                msg = checkEmail();
-                break;
-            case "date":
-                msg = checkDate();
-                break;
-        }
+    public static String checkDate() {
+        while (true) {
+            System.out.print("Date: ");
+            String date = sc.nextLine().trim();
+            if (!date.matches(DATE_REGEX)) {
+                System.out.println("Date must be format (dd/MM/yyyy)");
+                continue;
+            }
 
-        return msg;
+            String[] part = date.split("/");
+            int day = Integer.parseInt(part[0]);
+            int month = Integer.parseInt(part[1]);
+            int year = Integer.parseInt(part[2]);
+
+            if (month < 1 || month > 12) {
+                System.out.println("Month must be from 1 to 12");
+
+                continue;
+
+            }
+            if (month >= 1 && month <= 12) {
+                int[] dayOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                    dayOfMonth[1] = 29;
+                }
+
+                if (day < 1 || day > dayOfMonth[month - 1]) {
+                    System.out.println("Invalid day!");
+
+                    continue;
+                }
+            }
+            return date;
+
+        }
     }
-}
+
+    public static String inputWithValidation(String type) {
+        while (true) {
+            String msg = "";
+
+            switch (type.toLowerCase()) {
+                case "phone":
+                    msg = checkPhone();
+                    break;
+                case "email":
+                    msg = checkEmail();
+                    break;
+                case "date":
+                    msg = checkDate();
+                    break;
+            }
+
+            return msg;
+        }
+    }
 }
